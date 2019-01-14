4fd1ace8f457405b0ccdfd27131de498a39bd07f
tinhtinh95 <nttinh995@gmail.com>
2019-01-14 23:10:05 +0700

    sign and verify token

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,7 +1,17 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-var message = "I am user number 3";
-var hash = SHA256(message).toString();
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
 
-console.log(`Message: ${message}`);
-console.log(`Hash: `, hash);
+// console.log(`Message: ${message}`);
+// console.log(`Hash: `, hash);
+
+const data = {
+    id: 10
+}
+
+const token = jwt.sign(data, 'avc');
+console.log(token);
+
+console.log(jwt.verify(token, 'avc'));
