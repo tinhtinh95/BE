2a4140dc4364fe8fc50c1906407ac48d3e5c5c6b
Tina <tina@enclave.vn>
2019-01-14 13:47:31 +0700

    Updating a Resource - PATCH /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,6 +5,7 @@ const {ObjectId} =require('mongodb');
 const {mongoose} =require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const _ = require('lodash');
 
 var app = express();
 
@@ -64,6 +65,35 @@ app.delete('/todos/:id', (req, res)=> {
     }).catch(e => {
         return res.status(400).send();
     })
+})
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send('ID is invalid');
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id,
+        {
+            $set: body
+        }, {
+            new: true
+        }).then(todo => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch(e => {
+            res.status(400).send();
+        })
 })
 
 app.listen(port, () => {
