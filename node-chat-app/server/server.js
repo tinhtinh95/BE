ac0fa56602e50cceff62e160244fb83304da6a4e
Tina <tina@enclave.vn>
2019-01-21 11:40:02 +0700

    Adding Socket.io to an App

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,4 +1,8 @@
-const path = require('path')
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -6,12 +10,20 @@ const port = process.env.PORT || 3000;
 // console.log(publicPath);
 // vi thu muc public va server khac nhau nen dung path de goi den thu muc public
 
-const express = require('express');
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server); 
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => { // register event when event happened, client connected to server, socket argument is the same as index.html
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected'); // when close client
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server is ready at port: ${port}`);
 })
 
