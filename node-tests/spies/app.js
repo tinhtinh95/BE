a0a254593ca54e45a1b5f2a8b1c629e3c347a220
Tina <tina@enclave.vn>
2019-01-21 10:50:17 +0700

    Reset new branch

--- node-tests/spies/app.js
+++
@@ -1,9 +0,0 @@
-const db = require('./db.js');
-
-module.exports.handleSignUp = (email, password) => {
-    // Check if email already exists
-    // Save the user to the database
-    db.saveUser({email, password});
-    // Send the welcome email
-
-}
